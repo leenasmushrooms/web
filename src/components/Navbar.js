e375191b9592f0fc8c517e181a3302f68d1f07dd
jordankvarghese <tnufv8@gmail.com>
2025-01-04 18:34:13 +0530

    add new page courses

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,39 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
   const isBrowser = () => typeof window !== "undefined";
   const lng = isBrowser()
-  ? window.location.pathname.startsWith("/ml")
-    ? "/ml"
-    : ""
-  : "";
+    ? window.location.pathname.startsWith("/ml")
+      ? "/ml"
+      : ""
+    : "";
 
   return (
-    <nav
-      role="navigation"
-      aria-label="main-navigation"
-    >
-      <div className="container" style={{display:'flex', alignItems: 'center'}}>
+    <nav role="navigation" aria-label="main-navigation">
+      <div
+        className="container"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         <div className="navbar-brand">
-          <Link to={`${lng ?'/ml/' :'/'}`} className="navbar-item" title="Logo">
-            <img src={logo} alt="Leenas" style={{ width: "100px", height: "100px" }} />
+          <Link
+            to={`${lng ? "/ml/" : "/"}`}
+            className="navbar-item"
+            title="Logo"
+          >
+            <img
+              src={logo}
+              alt="Leenas"
+              style={{ width: "100px", height: "100px" }}
+            />
           </Link>
         </div>
-        <ul id="navMenu" style={{display: 'flex', marginLeft: 'auto', alignItems: 'center'}}>
+        <ul
+          id="navMenu"
+          style={{ display: "flex", marginLeft: "auto", alignItems: "center" }}
+        >
+          <li className="navbar-item" style={{ padding: "0px" }}>
+            <Link className="navbar-item" to={`${lng}/courses`}>
+              Courses
+            </Link>
+          </li>
           <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to={`${lng}/products`}>
               Products
@@ -35,7 +51,7 @@ const Navbar = () => {
             </Link>
           </li>
           <li className="navbar-end has-text-centered">
-            <LanguageSelector/>
+            <LanguageSelector />
           </li>
         </ul>
       </div>
